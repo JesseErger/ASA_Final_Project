2864ee4d1118987560218cff119ec9cb341088f2
jerger <jesse.erger@uiowa.edu>
2018-04-15 15:11:23 -0500

    removing input files and old vivado design

--- VS_Stripped_Raytracer/Raytracer/Raytracer.sdk/Raytracer/Debug/src/obj_parser.d
+++
@@ -1,8 +0,0 @@
-src/obj_parser.o src/obj_parser.o: ../src/obj_parser.c \
- ../src/obj_parser.h ../src/list.h ../src/string_extra.h
-
-../src/obj_parser.h:
-
-../src/list.h:
-
-../src/string_extra.h:
