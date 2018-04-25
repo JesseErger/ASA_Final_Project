5eafebbc14fd1d01fc79e8f6aad6e63e8e6f77c9
jerger <jesse.erger@uiowa.edu>
2018-04-24 23:18:51 -0500

    Adding a version of our raytracer integrated with HDMI output that builds locally but wont display on my monitor. To test on lab machines

---
+++ HDMI_With_Raytrace_SDK_Erger/sdk_1/HDMI_Raytrace_NOIP/Debug/src/main.d
@@ -0,0 +1,127 @@
+src/main.o src/main.o: ../src/main.c ../src/includes.h ../src/lodepng.h \
+ ../src/color.h ../src/material.h ../src/vector.h ../src/list.h \
+ ../src/sphere.h ../src/transforms.h ../src/lightRay.h ../src/main.h \
+ ../src/camera.h ../src/errorhandler.h ../src/filehandler.h \
+ ../src/renderer.h ../src/scene.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/display_ctrl/display_ctrl.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../src/display_ctrl/vga_modes.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xaxivdma.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xaxivdma_hw.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xaxivdma_i.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xdebug.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xvtc.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xvtc_hw.h \
+ ../src/display_ctrl/../dynclk/dynclk.h ../src/timer_ps/timer_ps.h \
+ ../src/intc/intc.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_exception.h \
+ ../src/video_capture/video_capture.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xgpio.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xgpio_l.h \
+ ../src/video_capture/../intc/intc.h
+
+../src/includes.h:
+
+../src/lodepng.h:
+
+../src/color.h:
+
+../src/material.h:
+
+../src/vector.h:
+
+../src/list.h:
+
+../src/sphere.h:
+
+../src/transforms.h:
+
+../src/lightRay.h:
+
+../src/main.h:
+
+../src/camera.h:
+
+../src/errorhandler.h:
+
+../src/filehandler.h:
+
+../src/renderer.h:
+
+../src/scene.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/display_ctrl/display_ctrl.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../src/display_ctrl/vga_modes.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xaxivdma.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xaxivdma_hw.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xaxivdma_i.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xdebug.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xvtc.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xvtc_hw.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+../src/timer_ps/timer_ps.h:
+
+../src/intc/intc.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_exception.h:
+
+../src/video_capture/video_capture.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xgpio.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xgpio_l.h:
+
+../src/video_capture/../intc/intc.h:
